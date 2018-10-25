5180b48013422c0748d93609b345d312c68f5452
Kristen Shea <kstar61@live.com>
2018-10-24 19:59:30 -0500

    *

--- Bonjour/Controllers/HomeController.cs
+++ Bonjour/Controllers/HomeController.cs
@@ -10,29 +10,19 @@ namespace Bonjour.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        //public IActionResult Index(string firstName, string lastName)
+        //{
+        //    return View();
+        //}
+
+        
+        public IActionResult Index(string firstName, string lastName)
         {
+            ViewBag.Firstname = firstName;
+            ViewBag.Lastname = lastName;
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
