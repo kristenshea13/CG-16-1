5ced38d207fa6c4de153d3f83f22a2b279c737bf
Kristen Shea <kstar61@live.com>
2018-10-25 19:40:50 -0500

    =

--- Bonjour/Controllers/HomeController.cs
+++ Bonjour/Controllers/HomeController.cs
@@ -1,36 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Bonjour.Models;
 using Microsoft.AspNetCore.Mvc;
-using Bonjour.Models;
+using System.Diagnostics;
 
 namespace Bonjour.Controllers
 {
     public class HomeController : Controller
     {
-        //public IActionResult Index(string firstName, string lastName)
-        //{
-        //    return View();
-        //}
-
-
+        
+        
         public IActionResult Index(string firstName)
         {
+            
             ViewBag.Firstname = firstName;
-            //ViewBag.Lastname = lastName;
+
             return View();
         }
 
-        //public IActionResult CreateMessage(string firstName)
+        //[HttpPost]
+        //public static string CreateMessage(string value, string firstName)
         //{
-        //    ViewBag.Firstname = firstName;
-
-        //    return View();
+            
 
         //}
-
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
